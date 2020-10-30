04d4c44dfa7eb3db541c5af0f1a958ce26304b5c
optimy <optimizion@gmail.com>
2020-10-30 20:45:59 +0900

    implements search layout - TopBar, Tags, BottomBar

--- app/src/main/java/com/swfusioncoding/instagramclone/BottomBar.kt
+++ app/src/main/java/com/swfusioncoding/instagramclone/BottomBar.kt
@@ -1,6 +1,10 @@
 package com.swfusioncoding.instagramclone
 
-class BottomBar {
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class BottomBar(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     // TODO : activity_search 안에 있는 include 안에 있는
     //  imageButton을 id로 접근 후 onClickListener 등록
 }
