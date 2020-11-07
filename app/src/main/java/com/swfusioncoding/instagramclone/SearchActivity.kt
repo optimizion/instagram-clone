e669722ad1053c65b3e268576870ba51018f362a
HyungJin Choi <optimizion@gmail.com>
2020-11-07 22:50:49 +0900

    add recycler view layout

--- app/src/main/java/com/swfusioncoding/instagramclone/SearchActivity.kt
+++ app/src/main/java/com/swfusioncoding/instagramclone/SearchActivity.kt
@@ -1,11 +1,44 @@
 package com.swfusioncoding.instagramclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val posts: ArrayList<Post> = addData()
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = SearchMainAdapter(posts)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerview_search_main).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+             setHasFixedSize(true)
+            // use a linear layout manager
+            layoutManager = viewManager
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
+    }
+
+    private fun addData(): ArrayList<Post> {
+        val posts: ArrayList<Post> = ArrayList()
+
+        for(i in 1..18) {
+            var post = Post()
+            post.mainImg = R.drawable.testimg
+            posts.add(post)
+        }
+
+        return posts
     }
 }
