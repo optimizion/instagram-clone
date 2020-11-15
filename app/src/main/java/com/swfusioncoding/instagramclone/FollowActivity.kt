a2086cce179151fc3c2f902864089af7ef4c0dab
LHR <48555909+LeeHeaRan@users.noreply.github.com>
2020-11-15 23:50:32 +0900

    환장..

--- app/src/main/java/com/swfusioncoding/instagramclone/FollowActivity.kt
+++ app/src/main/java/com/swfusioncoding/instagramclone/FollowActivity.kt
@@ -2,12 +2,30 @@ package com.swfusioncoding.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.swfusioncoding.instagramclone.Bean.FollowBean
 import com.swfusioncoding.instagramclone.R
+import com.swfusioncoding.instagramclone.adapter.FollowAdapter
 
 class FollowActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_follow)
+        
+//들어갈 데이터를 만드는 코드
+        var list = ArrayList<FollowBean>()
+        var bean1 = FollowBean()
+        bean1.userID = 
+
+
+//리싸이클러뷰 구현코드
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView_follow)
+        var layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = layoutManager
+        var adapter = FollowAdapter(list)
+        recyclerView.adapter = adapter
+
     }
 
     override fun getContentViewId(): Int {
--- app/src/main/java/com/swfusioncoding/instagramclone/adapter/FollowAdapter.kt
+++ app/src/main/java/com/swfusioncoding/instagramclone/adapter/FollowAdapter.kt
@@ -1,5 +1,49 @@
 package com.swfusioncoding.instagramclone.adapter
 
-class FollowAdapter {
-    // so happy -_-
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.swfusioncoding.instagramclone.Bean.FollowBean
+import com.swfusioncoding.instagramclone.R
+
+class FollowAdapter (var listData : ArrayList<FollowBean>)  : RecyclerView.Adapter<FollowAdapter.FollowHolder>()  {
+
+    override fun onBindViewHolder(holder: FollowHolder, position: Int) {
+        var bean = listData.get(position)
+//
+//        holder.mainImg?.setBackgroundColor(Color.RED)
+//        holder.userImg?.setBackgroundResource(bean!!.userImg!!)
+//
+//        holder.title?.setText(bean.title)
+//        holder.userName?.setText(bean.userName)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_follow, parent, false)
+        return FollowHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    class FollowHolder(var view: View) : RecyclerView.ViewHolder(view){
+//
+//        public var mainImg : ImageView? = null
+//        public var userImg : ImageView? = null
+//        public var title : TextView? = null
+//        public var userName : TextView? = null
+//
+//        init {
+//            mainImg = view.findViewById(R.id.mainImg)
+//            userImg = view.findViewById(R.id.userImg)
+//            title = view.findViewById(R.id.title)
+//            userName = view.findViewById(R.id.userName)
+//        }
+    }
 }
